7f53129da2a94629d512152d9ec7c03aa14f2e8f
Kagsoul <160343652+Kagsoul@users.noreply.github.com>
2025-03-27 16:28:47 -0300

    Update aula_02_01.py

--- AULA 02/aula_02_011.py
+++ AULA 02/aula_02_011.py
@@ -9,7 +9,7 @@ n2 = float(input(f'Informe a segunda nota do estudante {nome}: '))
 
 n3 = float(input(f'Informe a terceira nota do estudante {nome}: '))
 
-# Processamento dos Dados| fiz uma pergunta bem idiota, porque não percebi a importancia dos numero estarem dentro do () que faz os "n1, n2, n3" serem a nota somada e depois divida com /, percebi mais uma vez que o {:.1f} tira a poluição dos numero
+# Processamento dos Dados|
 
 media = (n1+n2+n3)/3
 
