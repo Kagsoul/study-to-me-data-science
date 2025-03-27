0ba5edb06543c04bf85c8e5c4a6aa7c1e3aff751
Kagsoul <160343652+Kagsoul@users.noreply.github.com>
2025-03-27 14:34:27 -0300

    Add files via upload

---
+++ idade.py
@@ -0,0 +1,23 @@
+
+
+nome = input ('qual e seu nome: ')
+wich_idade = float (input('qual e a sua idade: '))
+
+print (f'sua idade e {wich_idade:.0f} e voce se chama {nome}: ')
+
+idade = 29
+jovem = 18
+
+velho =69
+idoso = 30
+while (wich_idade == velho <= idoso ):
+    if velho or idoso:
+        print (f"você tem meia idade {wich_idade:.0f}")
+    break
+    
+      
+while (wich_idade == jovem >= idade ):
+    if jovem or idade:
+       print (f'voce e uma pessoa da idade jovem {wich_idade:.0f}')
+    break
+ 
